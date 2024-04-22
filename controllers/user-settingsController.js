25ca394d6a021b9574c93a296bd6faf3509ef47c
Mmah-Zombo <www.mariamzombo@gmail.com>
2024-04-22 12:39:09 +0000

    made the colors on the sidebar dependent on the route (dynamic)

--- controllers/user-settingsController.js
+++ controllers/user-settingsController.js
@@ -1,4 +1,7 @@
 module.exports = (req, res) => {
     user = authUser || null;
-    res.render('user-settings', {message: req.flash('message'), user});
+    let defaultColor  = 'cerise'; 
+    let selectedColor = 'antique-white';
+    const this_route = req.route.path;
+    res.render('user-settings', {message: req.flash('message'), user, defaultColor, selectedColor, this_route});
 }
